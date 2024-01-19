a5c9cabcedd6167fe3351400d36deda431ce4a4c
madhavgaba16 <mgaba_be23@thapar.edu>
2024-01-19 15:15:21 +0530

    first array question

---
+++ array.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <limits.h>
+using namespace std;
+// int main() {
+//     int arr[] = {1, 2, 3, 4, 5};
+//     int size = sizeof(arr) / sizeof(int);
+//     int min = INT_MAX;
+
+//     for (int i = 0; i < size; i++) {
+//         if (min<arr[i]) {
+//             min = arr[i];                              // finding miminum element in the array 
+            
+//         }
+//     }    
+
+//     std::cout << min << std::endl;
+//     return 0;
+
+int main(){
+    cout<<"hello world";
+    return 0;
+
+}
